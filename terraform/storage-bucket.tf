cc6b55b526af5d0008a707f34dec17777f85a62e
Serezha <rsv>
2024-04-05 00:34:54 +0300

    terrfaorm-2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-
   backend "s3" {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
