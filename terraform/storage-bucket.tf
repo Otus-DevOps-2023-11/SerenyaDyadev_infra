a8afa5dfd703cf26b0fc1a43436685811f2b1307
Serezha <rsv>
2024-04-05 00:07:55 +0300

    terrfaorm-2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,26 +1,18 @@
 terraform {
-  # required_providers {
-  #   yandex = {
-  #     source = "yandex-cloud/yandex"
-  #   }
-  # }
-
   backend "s3" {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
-    bucket = "<имя_бакета>"
+    bucket = "centhis-terraform-states"
     region = "ru-central1"
-    key    = "<путь_к_файлу_состояния_в_бакете>/<имя_файла_состояния>.tfstate"
+    key    = "stage.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+    skip_requesting_account_id  = true # необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
 
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1"
+    dynamodb_table    = "table980"
   }
-}
-
-provider "yandex" {
-  zone = "<зона_доступности_по_умолчанию>"
 }
